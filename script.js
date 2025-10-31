d9140f95a98ee0048cc77c7fb93f969cb63e16da
Arenjake-cpu <bautistaarenjake@gmail.com>
2025-10-31 14:53:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("scrollTop").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 80;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === #${current}) {
+      link.classList.add("active");
+    }
+  });
+});
